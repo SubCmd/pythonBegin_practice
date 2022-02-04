22ee6ead415020418762cb5ea638522f83f397d5
SubCmd <dumspiro.coding@gmail.com>
2022-02-04 23:17:22 +0900

    문자열 자료형 학습한 부분에 대한 실습 진행 사항

---
+++ Prac02.py
@@ -0,0 +1,42 @@
+#multistring.py
+'''
+print("=" * 50)
+print("My Program")
+print("=" * 50)
+'''
+
+#len func
+'''
+a = "Life is too short"
+b = len(a)
+print(b)
+'''
+
+#CodingAlone
+'''
+a = 'You need python'
+b = len(a)
+print(b)
+'''
+
+#exampleIndexing
+'''
+a = "Life is too short. You need Python"
+b = a[3]
+print (b)
+'''
+
+#exampleSlicing(1)
+'''
+a = "Life is too short. You need Python"
+b = a[0] + a[1] + a[2] + a[3]
+print(b)
+'''
+
+#exampleSlicing(2)
+'''
+a = "Pithon"
+#a[1] = 'y'
+b = a[:1] + 'y' + a[2:] 
+print(b)
+'''
